aeb60016de0d7d3dc7d3507a6f5467795e9dc972
GameGeek-Saikel <saikel-orado@foxmail.com>
2023-12-18 19:09:04 +0800

    Added a death message

---
+++ src/main/java/pers/saikel0rado1iu/sr/gen/data/DamageTypes.java
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 GameGeek-Saikel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package pers.saikel0rado1iu.sr.gen.data;
+
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.Registerable;
+import net.minecraft.registry.RegistryKey;
+import pers.saikel0rado1iu.silk.gen.data.SilkDamageType;
+import pers.saikel0rado1iu.sr.data.SpontaneousReplace;
+
+
+/**
+ * <h2 style="color:FFC800">自然更替的伤害类型生成器</h2>
+ *
+ * @author <a href="https://github.com/Saikel-Orado-Liu"><img alt="author" src="https://avatars.githubusercontent.com/u/88531138?s=64&v=4"></a>
+ */
+public class DamageTypes extends SilkDamageType {
+	public static final RegistryKey<DamageType> ACIDIZE = register(SpontaneousReplace.DATA, "acidize");
+	public static final RegistryKey<DamageType> SPIDER_TOXIN = register(SpontaneousReplace.DATA, "spider_toxin");
+	
+	@Override
+	public void bootstrap(Registerable<DamageType> damageTypeRegisterable) {
+		damageTypeRegisterable.register(ACIDIZE, new DamageType("acidize", 0));
+		damageTypeRegisterable.register(SPIDER_TOXIN, new DamageType("spider_toxin", 0));
+	}
+}
