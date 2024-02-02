77e621f5d0b9b0556214e67c3610c494adc6991d
GameGeek-Saikel <saikel-orado@foxmail.com>
2024-02-03 01:09:20 +0800

    update new armor implementation

--- src/main/java/pers/saikel0rado1iu/sr/data/SoundEvents.java
+++ src/main/java/pers/saikel0rado1iu/sr/data/SoundEvents.java
@@ -24,6 +24,7 @@
 
 package pers.saikel0rado1iu.sr.data;
 
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import pers.saikel0rado1iu.silk.api.registry.SilkSoundEvent;
@@ -34,10 +35,10 @@ import pers.saikel0rado1iu.silk.api.registry.SilkSoundEvent;
  * @author <a href="https://github.com/Saikel-Orado-Liu"><img alt="author" src="https://avatars.githubusercontent.com/u/88531138?s=64&v=4"></a>
  */
 public final class SoundEvents extends SilkSoundEvent {
-	public static final SoundEvent EQUIP_REFINED_COPPER = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.copper.equip"))).build();
-	public static final SoundEvent EQUIP_CUFE_ALLOY = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.cufe.equip"))).build();
-	public static final SoundEvent EQUIP_AUCU_ALLOY = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.aucu.equip"))).build();
-	public static final SoundEvent EQUIP_STEEL = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.steel.equip"))).build();
+	public static final RegistryEntry.Reference<SoundEvent> EQUIP_REFINED_COPPER = referenceBuilder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.copper.equip"))).build();
+	public static final RegistryEntry.Reference<SoundEvent> EQUIP_CUFE_ALLOY = referenceBuilder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.cufe.equip"))).build();
+	public static final RegistryEntry.Reference<SoundEvent> EQUIP_AUCU_ALLOY = referenceBuilder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.aucu.equip"))).build();
+	public static final RegistryEntry.Reference<SoundEvent> EQUIP_STEEL = referenceBuilder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.steel.equip"))).build();
 	public static final SoundEvent STONEBALL_THROW = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.stoneball.throw"))).build();
 	public static final SoundEvent SLINGSHOT_THROW = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.slingshot.throw"))).build();
 	public static final SoundEvent ARBALEST_LOADING_START = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.arbalest.loading.start"))).build();
@@ -57,8 +58,8 @@ public final class SoundEvents extends SilkSoundEvent {
 	public static final SoundEvent MARKS_CROSSBOW_QUICK_CHARGE_2 = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.marks_crossbow.quick_charge.2"))).build();
 	public static final SoundEvent MARKS_CROSSBOW_QUICK_CHARGE_3 = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.marks_crossbow.quick_charge.3"))).build();
 	public static final SoundEvent MARKS_CROSSBOW_SHOOT = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.marks_crossbow.shoot"))).build();
-	public static final SoundEvent EQUIP_ARROWPROOF_VEST = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.arrowproof_vest.equip"))).build();
+	public static final RegistryEntry.Reference<SoundEvent> EQUIP_ARROWPROOF_VEST = referenceBuilder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "vanilla.arrowproof_vest.equip"))).build();
 	public static final SoundEvent TREACHEROUS_SAC_BREAK = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "variant.treacherous_sac.break"))).build();
 	public static final SoundEvent SPRAY_TOXIN = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "spider.spray_poison_spider.spray_toxin"))).build();
-	public static final SoundEvent EQUIP_SPIDER_LEATHER_ARMOR = builder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "spider.spider_leather_armor.equip"))).build();
+	public static final RegistryEntry.Reference<SoundEvent> EQUIP_SPIDER_LEATHER_ARMOR = referenceBuilder(SoundEvent.of(new Identifier(SpontaneousReplace.DATA.getId(), "spider.spider_leather_armor.equip"))).build();
 }
